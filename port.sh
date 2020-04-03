63e61aaffd58dedb9fe1b538bc542754b4408adf
AL-AlamySploit <59588007+AL-AlamySploit@users.noreply.github.com>
2020-04-03 12:45:45 +0200

    Add files via upload

---
+++ port.sh
@@ -0,0 +1,54 @@
+c='\033[1;36m'
+g='\033[1;32m'
+r='\033[1;31m'
+y='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+z='\033[1;80m'
+clear
+echo -e $p"                          _"
+echo -e $g"         _ __   ___  _ __| |_"
+echo -e $r"        | '_ \ / _ \| '__| __|"
+echo -e $b"        | |_) | (_) | |  | |_"
+echo -e $b"        | .__/ \___/|_|   \__|"
+echo -e $b"        |_|"
+echo -e $p"         By:OOMD_NOOR"
+echo -e $y'###########################################'
+echo''
+termux-open-url https://youtu.be/h0_ojGWz8vY
+echo -e $c"{1} open port php"
+echo ''
+echo -e $c"{2} open port ngrok"
+echo ''
+echo -e $c"{0} Exit"
+echo ''
+echo -e -n $c"port>> "$y;read a
+echo ''
+if [ $a == 1 ];then
+echo -e -n $y"your ip>> "$g;read ip
+echo ''
+echo -e -n $y"your port>> "$g;read port
+echo ''
+echo -e "\033[1;31m[\033[1;32m*\033[1;31m] open port php"
+sleep 0.8
+php -S $ip:$port
+fi
+if [ $a == 2 ];then
+echo -e -n $y"type port>> "$g;read sor
+echo ''
+echo -e -n $y"your port>> "$g;read port
+echo ''
+echo -e "\033[1;31m[\033[1;32m*\033[1;31m] open port ngrok"
+sleep 0.8
+./ngrok $sor $port
+fi
+if [ $a == 0 ];then
+echo -e $g "thanks you"
+sleep 0.5
+echo ''
+exit
+
+else
+./port.sh
+fi
+
